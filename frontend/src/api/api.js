2f9a23e79e524a667cda6db243fbfc772b441e96
ajayupadhyay111 <upadhyayajay156@gmail.com>
2025-03-08 22:46:45 +0530

    urls changed

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useContext } from "react";
 
 export const API = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://full-stack-authentication-web-app.onrender.com/api",
   withCredentials: true,
 });
 
